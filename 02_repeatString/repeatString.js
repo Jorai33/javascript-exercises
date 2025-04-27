00b48ad94cd91cf0cc1a1ab071b6686aaf684cbb
Jorai33 <47177361+Jorai33@users.noreply.github.com>
2025-04-27 13:29:28 +0200

    feat: build the repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let output = "";
 
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      output += string;
+    }
+  } else {
+    output = "ERROR";
+  }
+
+  const repeatedString = output;
+
+  return repeatedString;
 };
 
 // Do not edit below this line
