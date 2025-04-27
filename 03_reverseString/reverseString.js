4ab46ef83ee36c12fd61569a1fcff4613378b679
Jorai33 <47177361+Jorai33@users.noreply.github.com>
2025-04-27 13:55:11 +0200

    feat: build the reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (message) {
+  let output = "";
 
+  for (let i = 1; i <= message.length; i++) {
+    output += message[message.length - i];
+  }
+
+  return output;
 };
 
 // Do not edit below this line
