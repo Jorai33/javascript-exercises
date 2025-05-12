943abca15bd4a851b8240bd81e7f95d80d812695
Jorai33 <47177361+Jorai33@users.noreply.github.com>
2025-05-12 21:00:11 +0200

    feat: build the sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  let difference = 0;
+  let hasNegativeNumbers = num1 < 0 || num2 < 0;
+  let isNotInteger = !Number.isInteger(num1) || !Number.isInteger(num2);
 
+  if (hasNegativeNumbers || isNotInteger) {
+    return "ERROR";
+  }
+
+  if (num1 < num2) {
+    difference = num2 - num1;
+    for (let i = 0; i <= difference; i++) {
+      sum += num1 + i;
+    }
+  } else {
+    difference = num1 - num2;
+    for (let i = 0; i <= difference; i++) {
+      sum += num2 + i;
+    }
+  }
+  const result = sum;
+  return result;
 };
 
 // Do not edit below this line
