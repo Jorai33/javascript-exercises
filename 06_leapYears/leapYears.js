d1f3ff00c548daf3e91d5b4a56abdd8aa7c5baf3
Jorai33 <47177361+Jorai33@users.noreply.github.com>
2025-05-12 21:48:36 +0200

    refactor: improve the function lisibility

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,13 @@
 const leapYears = function (year) {
-  let isLeapYear = false;
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleByFourHundred = year % 400 === 0;
 
-  if (
-    (year % 4 === 0 && year % 100 !== 0) ||
-    (year % 100 === 0 && year % 400 === 0)
-  ) {
-    isLeapYear = true;
+  if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)) {
+    return true;
+  } else {
+    return false;
   }
-
-  return isLeapYear;
 };
 
 // Do not edit below this line
